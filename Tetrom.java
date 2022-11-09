c767cbaa9fa0a14eae941197b08ee0ff20630352
janecj123 <janecj12345678@gmail.com>
2022-11-09 11:35:33 +0100

    vytvorenie Tetrom

---
+++ Tetrom.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of class Tetrom here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Tetrom {
+    /**
+     * Constructor for objects of class Tetrom
+     */
+    public Tetrom() {
+        // initialise instance variables
+    }
+}
