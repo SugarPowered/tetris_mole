3c2af1bbf738c02bc9e14f58e3547bbabf08b9f3
Michal Šubert <michal.subert22@gmail.com>
2022-11-09 11:29:55 +0100

    tetrisik

---
+++ tetrisik.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of class tetrisik here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class tetrisik {
+    /**
+     * Constructor for objects of class tetrisik
+     */
+    public tetrisik() {
+        // initialise instance variables
+    }
+}
